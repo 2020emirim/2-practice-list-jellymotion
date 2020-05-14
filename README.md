7f15d9cf6b8403fbd9e2ea3147ab52cd72c64f6f
jellymotion528 <jellymotion528@gmail.com>
2020-05-14 09:39:04 +0900

    Commit message

---
+++ README.md
@@ -0,0 +1,5 @@
+#practice list
+##bookmark
+- github 소스 -> pyCharm
+- bookmark 웹 만들기
+- models bookmark 만들기
---
+++ bookmark/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class bookmark(models.Model):
+    site_name = models.CharField(max_length=20)
+    url = models.URLField('Site URL')
+
+    def __str__(self):
+        return "제목: " + self.site_name + ", URL: " + self.url
