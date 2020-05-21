b1f48fa476809651ca01eedad3f4791ac0593ca6
jellymotion528 <jellymotion528@gmail.com>
2020-05-21 10:16:50 +0900

    templates bookmark_create

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
-from django.views.generic.list import ListView
-from .models import bookmark
+from django.views.generic import ListView, CreateView
+
+from bookmark.models import Bookmark
 
 
-class BookmarkListView(ListView):
-    model = bookmark
+class BookmarkList(ListView):  # bookmark_list.html
+    model = Bookmark
 
 
-class BookmarkCreateView(CreateView):
-    model = bookmark
-    fields = ['site_name', 'url']
-    success_url = reverse_lazy('list')
-    template_name_suffix = '_create'
+class BookmarkCreateView(CreateView):  # bookmark_form.html
+    model = Bookmark
+    fields = ['site_name', 'url']   #<form>
+    template_name_suffix = '_create'  # bookmark_create.html
+    success_url = reverse_lazy('bookmark:list')
