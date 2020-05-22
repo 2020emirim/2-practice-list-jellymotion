f7b6ede71cec670b2e48894b5059179ff68bda44
jellymotion528 <jellymotion528@gmail.com>
2020-05-22 15:00:12 +0900

    urls 'update/<iny:pk>/'

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -7,5 +7,5 @@ app_name = 'bookmark'
 urlpatterns = [
     path('', BookmarkList.as_view(), name='list'),  # {% url 'bookmark:list' %}
     path('add/', BookmarkCreateView.as_view(), name='add'),
-    path('detail/<int:pk>/', BookmarkDetailView.as_view(), name='detail')
+    path('detail/<int:pk>/', BookmarkDetailView.as_view(), name='detail') #집에가고싶다... 커밋이안되니까주석이라도 써본다
 ]
